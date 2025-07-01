8b189b46aae45579c04ca84e1eb5d046e003eeac
leowangsound <156690011+leowangsound@users.noreply.github.com>
2025-07-01 10:07:06 +0200

    Add files via upload

---
+++ klaro/annotated-config-file.js
@@ -0,0 +1,80 @@
+var klaroConfig = {
+    elementID: 'klaro',
+    storageMethod: 'cookie',
+    storageName: 'klaro',
+    htmlTexts: true,
+    cookieExpiresAfterDays: 180,
+    cookieDomain: 'leowangsound.de',
+    default: false,
+    mustConsent: true,
+    acceptAll: true,
+    hideDeclineAll: false,
+    hideLearnMore: false,
+
+    translations: {
+        de: {
+            privacyPolicyUrl: '/datenschutzerklaerung',
+            consentNotice: {
+                description: 'Hallo! Ich verwende Cookies und externe Inhalte (z. B. Spotify), um meine Website nutzungsfreundlich zu gestalten. Du kannst selbst entscheiden, was geladen werden darf.',
+            },
+            consentModal: {
+                title: 'Datenschutz-Einstellungen',
+                description: 'Hier kannst du einsehen und anpassen, welche externen Dienste auf dieser Seite eingebunden sind.',
+            },
+            purposes: {
+                media: {
+                    title: 'Externe Medien',
+                    description: 'Einbindung von Spotify, YouTube o. ä.',
+                },
+                analytics: {
+                    title: 'Besucher-Statistiken',
+                },
+            }
+        }
+    },
+
+    services: [
+        {
+            name: 'spotify',
+            title: 'Spotify Embed',
+            purposes: ['media'],
+            cookies: [],
+            contextualConsentOnly: true,
+            translations: {
+                de: {
+                    description: 'Spotify wird verwendet, um Inhalte direkt auf der Website einzubetten.',
+                }
+            }
+        },
+        {
+            name: 'youtube',
+            title: 'YouTube Embed',
+            purposes: ['media'],
+            contextualConsentOnly: true,
+            translations: {
+                de: {
+                    description: 'YouTube wird verwendet, um Videos direkt auf der Website einzubetten.',
+                }
+            }
+        }
+        // Optional:
+        // {
+        //     name: 'googlefonts',
+        //     title: 'Google Fonts',
+        //     purposes: ['styling'],
+        //     required: false,
+        //     onlyOnce: true,
+        //     translations: {
+        //         de: {
+        //             description: 'Webfonts von Google werden zur Darstellung verwendet (nicht lokal eingebunden).',
+        //         }
+        //     }
+        // }
+    ],
+
+    callback: function(consent, service) {
+        console.log(
+            'User consent for service ' + service.name + ': consent=' + consent
+        );
+    },
+};
